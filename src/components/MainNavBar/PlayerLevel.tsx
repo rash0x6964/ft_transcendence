fc33330fadead8a89457a9688f3455fc445f8b65
Hatim Rachid <hat83203@gmail.com>
2023-09-21 22:16:07 +0100

    adding blur to player level

--- src/components/MainNavBar/PlayerLevel.tsx
+++ src/components/MainNavBar/PlayerLevel.tsx
@@ -19,6 +19,10 @@ export default function PlayerLevel({percentage,level,className}:Props) {
 						<div className="w-0 h-0 border-l-4 border-l-transparent border-t-4 border-primary border-r-4 border-r-transparent right-[-2px] mx-auto "></div>
 					</div>
 
+					<div className="w-full  absolute bottom-0 blur-[2px] ">
+						<div className="w-0 h-0 border-l-4 border-l-transparent border-t-4 border-primary border-r-4 border-r-transparent right-[-2px] mx-auto "></div>
+					</div>
+
 
 				</div>
 				<div className="w-28 h-2 bg-secondary-400 my-auto rounded relative">

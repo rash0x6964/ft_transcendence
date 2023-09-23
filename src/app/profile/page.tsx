ceb04ef1360ade00cf4f8b1dee549ef158fc3780
samini <soufianeamini2002@gmail.com>
2023-09-23 14:27:18 +0100

    added profile route

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image"
+
+export default function page() {
+  return (
+    <div>
+      <img
+        className="blur"
+        src="https://www.mobafire.com/images/champion/skins/landscape/yasuo-sea-dog-762x.jpg"
+        alt="backdrop"
+      />
+      <img
+        className="h-1/6 w-1/6"
+        src="https://pxbar.com/wp-content/uploads/2023/09/anime-profile-pictures.jpg"
+        alt="profile picture"
+      />
+      profile
+    </div>
+  )
+}

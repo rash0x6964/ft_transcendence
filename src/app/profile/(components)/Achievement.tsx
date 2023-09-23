21275e5178697797f2ca619279c519cad4f9cf28
samini <soufianeamini2002@gmail.com>
2023-09-23 15:35:17 +0100

    basic achievements component

--- src/app/profile/(components)/Achievement.tsx
+++ src/app/profile/(components)/Achievement.tsx
@@ -1,11 +1,12 @@
 type Props = {
   name: string
 }
+
 export default function Achievement({ name }: Props) {
   return (
     <>
       <img
-        className="h-24 w-24"
+        className="h-24 w-24 p-3 rounded-full"
         src="https://png.pngtree.com/png-vector/20190429/ourmid/pngtree-vector-trophy-icon-png-image_997143.jpg"
         alt={name}
       />
--- src/app/profile/(components)/Achievements.tsx
+++ src/app/profile/(components)/Achievements.tsx
@@ -2,24 +2,28 @@ import Achievement from "./Achievement"
 
 export default function Achievements() {
   const achievements = [
-    {
-      name: "one",
-    },
-    {
-      name: "two",
-    },
-    {
-      name: "three",
-    },
-    {
-      name: "four",
-    },
+    { name: "one" },
+    { name: "two" },
+    { name: "three" },
+    { name: "four" },
+    { name: "five" },
+    { name: "six" },
+    { name: "seven" },
+    { name: "eight" },
+    { name: "nine" },
+    { name: "ten" },
+    { name: "eleven" },
+    { name: "twelve" },
+    { name: "thirteen" },
+    { name: "fourteen" },
+    { name: "fifteen" },
   ]
+
   return (
     <>
-      <div className="bg-secondary">
+      <div className="bg-secondary rounded-2xl max-w-lg">
         <h2 className="font-semibold p-3">Achievements</h2>
-        <div className="flex">
+        <div className="flex flex-wrap justify-center">
           {achievements.map((achievement) => {
             return (
               <Achievement key={achievement.name} name={achievement.name} />

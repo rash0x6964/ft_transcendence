139bfaa794beeb2e48b89156377625fb5d335ef0
samini <soufianeamini2002@gmail.com>
2023-09-24 18:52:35 +0100

    .

--- src/app/profile/(components)/LevelCube.tsx
+++ src/app/profile/(components)/LevelCube.tsx
@@ -10,7 +10,7 @@ export default function LevelCube() {
   }
   return (
     <div className="flex items-center">
-      <div className=" gradient-stop flex justify-center items-center bg-slate-800 border-slate-50 border-[1px] w-14 h-14 rotate-45">
+      <div className=" flex justify-center items-center bg-slate-800 border-slate-50 border-[1px] w-14 h-14 rotate-45">
         <div className="-rotate-45 font-semibold text-xl">10</div>
       </div>
     </div>

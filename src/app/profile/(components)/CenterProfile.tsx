9551ee18a1097c41bafc693f37db4d70af011863
samini <soufianeamini2002@gmail.com>
2023-09-26 19:50:06 +0100

    added profile

--- src/app/profile/(components)/CenterProfile.tsx
+++ src/app/profile/(components)/CenterProfile.tsx
@@ -1,7 +1,13 @@
 export default function CenterProfile() {
+  const url =
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBLE2R35SV62Enw03QHS5AY-LUr6HOhmHvrA&usqp=CAU"
+
   return (
     <>
-      <div className="font-semibold text-xl">rash0x6964</div>
+      <div className="flex flex-col items-center w-40 pr-5">
+        <img className="rounded-full" src={url} alt="Profile picture" />
+        <div className="font-semibold text-xl">rash0x6964</div>
+      </div>
     </>
   )
 }

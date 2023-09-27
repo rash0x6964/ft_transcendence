66bfcaa88b1be21d3602ff7557261ceb080212b6
Hatim Rachid <hat83203@gmail.com>
2023-09-27 17:31:59 +0100

    adjusting avatar

--- src/components/BaseComponents/Avatar.tsx
+++ src/components/BaseComponents/Avatar.tsx
@@ -1,26 +1,28 @@
 import Image from "next/image"
 
 type Props = {
-  className?: string
-  src: string
-  alt?: string
-  onClick?: () => void
+	className?: string
+	src: string
+	alt?: string
+	onClick?: () => void,
+	override?: boolean
 }
 
 export default function Avatar({
-  className,
-  src,
-  alt = "haha",
-  onClick,
+	className,
+	src,
+	alt = "haha",
+	onClick,
+	override = false
 }: Props) {
-  return (
-    <Image
-      onClick={onClick}
-      className={`rounded-full  object-cover   drop-shadow-lg  ${className}`}
-      src={src}
-      alt={alt}
-	  width={250}
-	  height={250}
-    />
-  )
+	return (
+		<Image
+			onClick={onClick}
+			className={` ${!override && "rounded-full"}  object-cover   drop-shadow-lg  ${className}`}
+			src={src}
+			alt={alt}
+			width={250}
+			height={250}
+		/>
+	)
 }

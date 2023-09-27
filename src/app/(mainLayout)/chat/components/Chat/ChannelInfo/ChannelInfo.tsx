fde3cfdcbc4379b42dddef8d68c60cf1177f83ae
Hatim Rachid <hat83203@gmail.com>
2023-09-27 13:07:57 +0100

    sperating context menu logic

--- src/app/(mainLayout)/chat/components/Chat/ChannelInfo/ChannelInfo.tsx
+++ src/app/(mainLayout)/chat/components/Chat/ChannelInfo/ChannelInfo.tsx
@@ -2,40 +2,21 @@
 import React, { CSSProperties, useEffect, useRef, useState } from "react"
 import Avatar from "@/components/BaseComponents/Avatar"
 import MemberCard from "./MemberCard"
-import ContextMenu from "@/components/BaseComponents/ContextMenu"
+import ContextMenu, { useContextMenu } from "@/components/BaseComponents/ContextMenu"
 import { MouseEvent } from "react"
 import { MenuBtn } from "@/components/BaseComponents/ContextMenu"
+import { getMenuPos } from "@/components/BaseComponents/ContextMenu"
 
 export default function ChannelInfo() {
 	const adminsCount = 4
-	const [position, setPosition] = useState({ x: 0, y: 0 })
-	const [clicked, setClicked] = useState<boolean>(false);
+
 	const menuRef = useRef<HTMLDivElement>(null);
+	const [position, setPosition] = useState({ x: 0, y: 0 })
+	const [clicked, setClicked] = useContextMenu(menuRef);
 
-	useEffect(() => {
-		let handler = (e: Event) => {
-			if (!menuRef.current?.contains(e.target as Node)) {
-				setClicked(false)
-			}
-		}
-		document.addEventListener("mousedown", handler);
-		() => {
-			document.removeEventListener("mousedown", handler)
-		}
-	}, [])
 	const handleContextMenu = (e: MouseEvent<HTMLDivElement>, PlayerName: string) => {
-		console.log(menuRef);
 		setClicked(true);
-		let posX = e.clientX;
-		let posY = e.clientY;
-		if (!menuRef.current)
-			return;
-
-		if (posY + menuRef.current?.clientHeight > window.innerHeight)
-			posY -= menuRef.current?.clientHeight;
-		if (posX + menuRef.current.clientWidth > window.innerWidth)
-			posX -= menuRef.current.clientWidth;
-		setPosition({ x: posX, y: posY });
+		setPosition(getMenuPos(e, menuRef));
 	}
 
 	return (
@@ -60,8 +41,8 @@ export default function ChannelInfo() {
 					playerName="rash0x6964"
 					playerState="In queue" />
 			</div>
-			<ContextMenu MenuRef={menuRef} style={{ top: position.y, left: position.x }} className={`fixed top-0 left-0 ${!clicked && "hidden"} `} >
-				<MenuBtn title="Profile" />
+			<ContextMenu MenuRef={menuRef} style={{ top: position.y, left: position.x }} className={` ${!clicked && "hidden"} `} >
+				<MenuBtn onClick={()=>alert("yes")} title="Profile" />
 				<MenuBtn title="Kick" />
 				<MenuBtn title="Mute" />
 				<MenuBtn title="Ban" />
--- src/components/BaseComponents/ContextMenu.tsx
+++ src/components/BaseComponents/ContextMenu.tsx
@@ -1,13 +1,13 @@
 import React, { Children, PropsWithChildren } from 'react'
 import { CSSProperties } from 'react'
-
-
+import { MouseEvent } from 'react';
+import { useState, useEffect } from 'react';
 type Props =
-{
-	className?: string
-	style?: CSSProperties
-	MenuRef:React.LegacyRef<HTMLDivElement>
-}
+	{
+		className?: string
+		style?: CSSProperties
+		MenuRef: React.LegacyRef<HTMLDivElement>
+	}
 
 export function MenuBtn({ title, onClick }: { title: string; onClick?: () => void }) {
 	return (
@@ -20,12 +20,43 @@ export function MenuBtn({ title, onClick }: { title: string; onClick?: () => voi
 	)
 }
 
-export default function ContextMenu({className,style,MenuRef,children}: Props  & PropsWithChildren) {
+export default function ContextMenu({ className, style, MenuRef, children }: Props & PropsWithChildren) {
 	return (
 		<div ref={MenuRef}
 			style={style}
-			className={"flex flex-col p-3 w-fit bg-secondary rounded border border-gray-600 fixed z-50 " + className}	>
+			className={"fixed top-0 left-0 flex flex-col p-3 w-fit bg-secondary rounded border border-gray-600  z-50 " + className}	>
 			{children}
 		</div>
 	)
+}
+
+export function getMenuPos(e: MouseEvent, menuRef: any): { x: number, y: number } {
+	let posX: number = e.clientX;
+	let posY: number = e.clientY;
+	if (!menuRef.current)
+		return { x: 0, y: 0 };
+
+	if (posY + menuRef.current?.clientHeight > window.innerHeight)
+		posY -= menuRef.current?.clientHeight;
+	if (posX + menuRef.current.clientWidth > window.innerWidth)
+		posX -= menuRef.current.clientWidth;
+	return { x: posX, y: posY }
+}
+
+
+export function useContextMenu(menuRef: any): [isClicked: boolean, setClicked: React.Dispatch<React.SetStateAction<boolean>>] {
+	const [isClicked, setClicked] = useState<boolean>(false);
+	useEffect(() => {
+		let handler = (e: Event) => {
+			if (!menuRef.current?.contains(e.target as Node)) {
+				setClicked(false)
+			}
+		}
+		document.addEventListener("mousedown", handler);
+		() => {
+			document.removeEventListener("mousedown", handler)
+		}
+	}, [])
+
+	return [isClicked, setClicked];
 }

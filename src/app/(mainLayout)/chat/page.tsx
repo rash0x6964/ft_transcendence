31c1a2a932326ed11cee8b6e330185e70a7cdf53
Soufiane Amini <samini@e3r7p8.1337.ma>
2023-09-28 12:52:50 +0100

    fix import

--- src/app/(mainLayout)/chat/page.tsx
+++ src/app/(mainLayout)/chat/page.tsx
@@ -4,8 +4,8 @@ import Chat from "./components/Chat"
 import FriendInfo from "./components/FriendInfo/FriendInfo"
 import ChannelInfo from "./components/Chat/ChannelInfo/ChannelInfo"
 import Dialogue from "@/components/Dialogue/Dialogue"
-import CreateChannelDialBox from "./components/ChatBar/CreateChannelDialogueBox/CreateChannelDialBox"
-import FriendRequestsDialBox from "./components/ChatBar/CreateChannelDialogueBox/FriendRequestsDialBox"
+import FriendRequestsDialBox from "./components/ChatBar/DialogueBoxes/FriendRequestsDialBox"
+import CreateChannelDialBox from "./components/ChatBar/DialogueBoxes/CreateChannelDialBox"
 
 export default function Page() {
   return (
@@ -20,7 +20,7 @@ export default function Page() {
         <ChannelInfo />
       </div>
       <Dialogue closed={false}>
-        {/* <CreateChannelDialBox /> */}
+        <CreateChannelDialBox />
         <FriendRequestsDialBox />
       </Dialogue>
     </div>

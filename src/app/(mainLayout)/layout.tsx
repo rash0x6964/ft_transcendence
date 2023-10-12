0eebff62f3c87caced181f5d6f5e73486a736b62
Hatim Rachid <hat83203@gmail.com>
2023-10-12 18:53:55 +0100

    mooving certain tags from mainlayout fo a component due to client interactivity

--- src/app/(mainLayout)/layout.tsx
+++ src/app/(mainLayout)/layout.tsx
@@ -2,9 +2,7 @@
 import "../globals.css"
 import type { Metadata } from "next"
 import { Lexend } from "next/font/google"
-import MainNavBar from "@/components/MainNavBar/MainNavBar"
-import LeftBar from "@/components/LeftBar/LeftBar"
-import RightBar from "@/components/RightBar/RightBar"
+import MainView from "./(components)/MainView"
 const lexend = Lexend({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
@@ -22,20 +20,10 @@ export default function RootLayout({
 			<body className={lexend.className}>
 
 				<div className="w-screen fixed h-screen  bg-gradient-to-r from-10% to-80% from-backdrop to-mirage -z-10 "></div>
+				<MainView>
+					{children}
+				</MainView>
 
-				<div className="  h-screen w-screen flex flex-col ">
-					<MainNavBar className=" w-screen" coins={8000} />
-					<div className="flex-1 flex justify-between mb-4">
-
-						<LeftBar className=" h-full w-fit    bg-secondary mx-2 py-6 drop-shadow-lg  mb-4 rounded-lg flex flex-col  justify-between" />
-						<div className=" flex-1 relative">
-							<div className="absolute w-full h-full max-h-full overflow-y-auto">
-								{children}
-							</div>
-						</div>
-						<RightBar className="shadow  h-full drop-shadow-lg   w-16 bg-secondary mx-2 mb-4 rounded-lg flex flex-col items-center gap-5 py-5" />
-					</div>
-				</div>
 			</body>
 		</html>
 	)

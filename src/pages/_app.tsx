41e0cca4097a8c0fe33fbbd62c48538921607fcc
Hatim Rachid <hat83203@gmail.com>
2023-10-19 22:35:42 +0100

    Disabling SSR Completly

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import type { ReactElement, ReactNode } from 'react'
 import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
 import "@/globals.css"
+import dynamic from 'next/dynamic'
+
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode
 }
@@ -9,10 +11,14 @@ export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout
 }
-
-export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? ((page) => page)
 
   return getLayout(<Component {...pageProps} />)
 }
+
+
+export default  dynamic(() => Promise.resolve(MyApp), {
+	ssr: false,
+  })

1a5ee27a610d61231bc3cb30f51a6de5a86145e7
Hatim Rachid <hat83203@gmail.com>
2023-10-26 22:04:34 +0100

    emitting event on message send

--- src/UI/game/chat/Chat.tsx
+++ src/UI/game/chat/Chat.tsx
@@ -1,21 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Header from "./Chat/Header";
 import ChatInputs from "./Chat/ChatInputs";
 import MainChat from "./Chat/MainChat";
 import Message from "@/models/Message.model";
 import MessageService from "@/services/Message.service";
+import { WebSocketContext } from "@/UI/WebSocketContextWrapper";
+import { getJwtCookie } from "@/services/CookiesService";
 export default function Chat() {
+	const socket = useContext(WebSocketContext);
 	const [messages, setMessages] = useState<Message[]>([]);
+
 	let id: string = "clo79v0gp0001u5mg1ak3jwwi"
+
 	useEffect(() => {
 		MessageService.getDmMessages(id).then((data) => {
 			setMessages(data.data);
 		})
+
+
+		socket?.on("privateMessage", (data) => {
+			setMessages((prevState) => [...prevState, data.data])
+		})
 	}, [])
+
 
 	const handleSend = (val: string) => {
 		MessageService.sendDmMessage(val, id).then(data => {
-
+			socket?.emit("privateMessage", { token: getJwtCookie(), data: data.data });
 			setMessages((prevState) => [...prevState, data.data])
 		}).catch(err => {
 

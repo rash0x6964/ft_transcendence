ce78022ef49754ec4b13f9b27ecd9f84935cf546
Soufiane Amini <samini@e1r3p7.1337.ma>
2023-10-30 15:58:49 +0100

    display match history from database

--- src/services/MatchService.ts
+++ src/services/MatchService.ts
@@ -6,20 +6,50 @@ import MatchDisplayData from "@/types/MatchDisplayData"
 class MatchService {
   private endpoint = "/match"
 
-  getAllMatches() {
-    return HttpClient.get(`${this.endpoint}`)
+  getAllMatches(): Promise<Match[]> {
+    return HttpClient.get(`${this.endpoint}`).then(result => result.data)
   }
 
   async getMatchProps(matches: Match[]): Promise<MatchDisplayData[]> {
+    function datediff(first: number, second: number) {
+      return Math.round((second - first) / (1000 * 60 * 60 * 24))
+    }
     const current = await getCurrent()
 
-    return matches.map(async match => {
-      const enemyId =
-        match.winnerID == current.id ? match.loserID : match.winnerID
-      const enemy = await getById(enemyId)
+    console.log(matches)
 
-      return {}
-    })
+    const promises: Promise<MatchDisplayData>[] = matches.map(
+      async (match): Promise<MatchDisplayData> => {
+        const enemyId =
+          match.winnerID === current.id ? match.loserID : match.winnerID
+        const enemy = await getById(enemyId)
+        const days = datediff(new Date(match.date).getTime(), Date.now())
+
+        return Promise.resolve({
+          p_1: {
+            username: current.userName,
+            url: current.avatarUrl,
+          },
+          p_2: {
+            username: enemy.userName,
+            url: enemy.avatarUrl,
+          },
+          s_1:
+            match.winnerID === current.id
+              ? match.winnerScore
+              : match.loserScore,
+          s_2:
+            match.winnerID === enemy.id ? match.winnerScore : match.loserScore,
+          win: current.id === match.winnerID ? true : false,
+          type: match.ranked ? "Ranked" : "Normal",
+          days,
+          name: "Game",
+        })
+      }
+    )
+
+    const matchesDisplayData = Promise.all(promises)
+    return matchesDisplayData
   }
 }
 

d49b2513ee8da0524d0a04dd923167953a90f203
samini <soufianeamini2002@gmail.com>
2023-11-01 20:05:28 +0100

    better display for match history

--- src/UI/game/profile/(matches)/MatchInfo.tsx
+++ src/UI/game/profile/(matches)/MatchInfo.tsx
@@ -7,11 +7,15 @@ type Props = {
 export default function MatchInfo({ type, win, days }: Props) {
   const outcome = win ? "Win" : "Loss"
   let className = win ? "text-emerald-600" : "text-red-600"
+  let daysMessage = `${days} Days ago`
+
+  if (days === 0) daysMessage = "Today"
+  else if (days === 1) daysMessage = "1 Day ago"
 
   return (
     <div className="flex justify-between flex-col items-center">
       <h3 className="font-semibold text-sm">{type}</h3>
-      <h6 className="text-gray-400 text-xs">{days} Days ago</h6>
+      <h6 className="text-gray-400 text-xs">{daysMessage}</h6>
       <h5 className={className + " text-sm"}>{outcome}</h5>
     </div>
   )

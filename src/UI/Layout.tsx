3184e8e7fbb28ccf4488bbe33e12212ca35617b7
Hatim Rachid <hat83203@gmail.com>
2023-11-02 14:56:02 +0100

    adding user context fixing ui bugs

--- src/UI/Layout.tsx
+++ src/UI/Layout.tsx
@@ -7,30 +7,35 @@ import AuthProvider from "./AuthProvider"
 import WebSocketContextProvider from "./WebSocketContextWrapper"
 import Notification from "@/components/BaseComponents/Notification"
 import NotificationProvider from "./NotificationProvider"
+import ActiveUserProvider from "./ActiveUserProvider"
 const lexend = Lexend({ subsets: ["latin"] })
 export default function Layout({ children }: PropsWithChildren) {
 	return (
 		<AuthProvider>
-			<WebSocketContextProvider>
+			<ActiveUserProvider>
+				<WebSocketContextProvider>
 
-				<main className={lexend.className}>
-					<div className="w-screen fixed h-screen  bg-gradient-to-r from-10% to-80% from-backdrop to-mirage -z-10 "></div>
 
-					<div className="  h-screen w-screen flex flex-col ">
-						<MainNavBar className=" w-screen" coins={8000} />
-						<div className="flex-1 flex justify-between mb-4">
-							<LeftBar className=" h-full w-fit    bg-secondary mx-2 py-6 drop-shadow-lg  mb-4 rounded-lg flex flex-col  justify-between" />
-							<div className=" flex-1 relative">
-								<div className="absolute w-full h-full max-h-full overflow-y-auto">
-									{children}
+					<main className={lexend.className}>
+						<div className="w-screen fixed h-screen  bg-gradient-to-r from-10% to-80% from-backdrop to-mirage -z-10 "></div>
+
+						<div className="  h-screen w-screen flex flex-col ">
+							<MainNavBar className=" w-screen" coins={8000} />
+							<div className="flex-1 flex justify-between mb-4">
+								<LeftBar className=" h-full w-fit    bg-secondary mx-2 py-6 drop-shadow-lg  mb-4 rounded-lg flex flex-col  justify-between" />
+								<div className=" flex-1 relative">
+									<div className="absolute w-full h-full max-h-full overflow-y-auto">
+										{children}
+									</div>
 								</div>
+								<RightBar className="shadow  h-full drop-shadow-lg   w-16 bg-secondary mx-2 mb-4 rounded-lg flex flex-col items-center gap-5 py-5" />
 							</div>
-							<RightBar className="shadow  h-full drop-shadow-lg   w-16 bg-secondary mx-2 mb-4 rounded-lg flex flex-col items-center gap-5 py-5" />
 						</div>
-					</div>
-				</main>
+					</main>
+				</WebSocketContextProvider >
+			</ActiveUserProvider>
 
-			</WebSocketContextProvider >
+
 		</AuthProvider >
 	)
 }

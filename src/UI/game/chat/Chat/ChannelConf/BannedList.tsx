ccfe1e830f81e15d3a65373895cd2ab5dc34838a
rash0x6964 <rash0x6964@gmail.com>
2023-11-12 17:22:25 +0100

    block list designed

--- src/UI/game/chat/Chat/ChannelConf/BannedList.tsx
+++ src/UI/game/chat/Chat/ChannelConf/BannedList.tsx
@@ -28,76 +28,82 @@ export default function BannedList({ channelId }: { channelId: string }) {
       status: "FREE",
     }
 
-    ChannelUserService.free(data).then((res) => {
-      setBannedList(bannedList.filter((x) => x.userID != id))
-      socket?.emit('getUnbanned', {
+    ChannelUserService.free(data)
+      .then((res) => {
+        setBannedList(bannedList.filter((x) => x.userID != id))
+        socket?.emit("getUnbanned", {
           token: cookieService.getJwtCookie(),
           data: res.data,
         })
-    }).catch((err) => {
-      // console.log(err)
-    })
+      })
+      .catch((err) => {
+        // console.log(err)
+      })
   }
 
   return (
     <div className="">
-      <div className="flex flex-col ">
-        <div className="h-14 w-fill flex pl-8 pr-16 text-sm text-slate-600">
-          <div className="flex justify-around">
-            <span className="self-center pl-10 pr-52">Member</span>
-            <span className="self-center pr-24">Join Date</span>
-            <span className="self-center pr-20">Member Since</span>
-            <span className="self-center">Unblock</span>
-          </div>
-        </div>
-        <div className="flex flex-col gap-3 bg-secondary rounded-xl gradient-border-2 px-5 pb-5 pt-5">
-          {bannedList.map((item, index) => {
-            // if (index > 2)
-            const to_year =
-              new Date(Date.now()).getFullYear() -
-              new Date(item.joinedAt).getFullYear()
+      <div className="flex flex-col gap-3 bg-secondary rounded-xl gradient-border-2 px-5 pb-5 pt-5">
+        <p>Banned members</p>
+        <br />
+        {bannedList.length > 0 ? <div className="h-14 w-fill flex text-sm text-slate-600">
+          <span className="self-center pl-5 pr-60">Member</span>
+          <span className="self-center pr-24">Join Date</span>
+          <span className="self-center pr-20">Member Since</span>
+          {/* <span className="self-center">Unblock</span> */}
+        </div> : <p className="text-center opacity-25">No one blocked</p>}
 
-            return (
-              <div className="h-14 w-fill bg-secondary drop-shadow-lg rounded-lg flex pl-7 pr-16 text-sm ">
-                <div className="flex  justify-around flex-1">
-                  {/* Member */}
-                  <div className="self-center flex gap-3">
-                    <Avatar
-                      key={item.userID}
-                      className="w-8 h-8"
-                      src={item.user?.avatarUrl}
-                      alt={item.user?.userName}
-                    />
-                    <span className="self-center">{item.user?.userName}</span>
-                  </div>
+        {bannedList.map((item, index) => {
+          // if (index > 2)
+          const to_year =
+            new Date(Date.now()).getFullYear() -
+            new Date(item.joinedAt).getFullYear()
 
-                  {/* Join Date */}
-                  <div className="self-center flex gap-3">
-                    <span className="self-center">
-                      {`${new Date(item.joinedAt).getFullYear()}/${new Date(
-                        item.joinedAt
-                      ).getMonth()}/${new Date(item.joinedAt).getDay()}`}
-                    </span>
-                  </div>
+          return (
+            <div className="h-14 w-fill bg-secondary drop-shadow-lg rounded-lg flex pl-7 pr-16 text-sm ">
+              <div className="flex  justify-between flex-1">
+                {/* Member */}
+                <div className="self-center flex gap-3">
+                  <Avatar
+                    key={item.userID}
+                    className="w-8 h-8"
+                    src={item.user?.avatarUrl}
+                    alt={item.user?.userName}
+                  />
+                  <span className="self-center">{item.user?.userName}</span>
+                </div>
 
-                  {/* Member Since */}
-                  <div className="self-center flex gap-3">
-                    <span>{to_year} Years ago</span>
-                  </div>
+                {/* Join Date */}
+                <div className="self-center flex gap-3">
+                  <span className="self-center">
+                    {`${new Date(item.joinedAt).getFullYear()}/${new Date(
+                      item.joinedAt
+                    ).getMonth()}/${new Date(item.joinedAt).getDay()}`}
+                  </span>
+                </div>
 
-                  {/* Unblock */}
-                  <div className="self-center flex gap-3">
-                    {/* <button className="p-3"> */}
-                    <button className="p-3 rounded-md bg-slate-800" onClick={() => {unblock && unblock(item.userID)}}>
-                      <UnblockAtList key={item.userID} width={16} height={16} />
-                    </button>
-                    {/* </button> */}
-                  </div>
+                {/* Member Since */}
+                <div className="self-center flex gap-3">
+                  <span>{to_year} Years ago</span>
+                </div>
+
+                {/* Unblock */}
+                <div className="self-center flex gap-3">
+                  {/* <button className="p-3"> */}
+                  <button
+                    className="p-3 rounded-md bg-slate-800"
+                    onClick={() => {
+                      unblock && unblock(item.userID)
+                    }}
+                  >
+                    <UnblockAtList key={item.userID} width={16} height={16} />
+                  </button>
+                  {/* </button> */}
                 </div>
               </div>
-            )
-          })}
-        </div>
+            </div>
+          )
+        })}
       </div>
     </div>
   )

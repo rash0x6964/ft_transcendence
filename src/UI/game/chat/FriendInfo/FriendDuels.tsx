09f5f5e97b65a001774ec2acb19d14160e95bef9
rash0x6964 <rash0x6964@gmail.com>
2023-11-18 17:48:47 +0100

    fixed initial err msg appearance

--- src/UI/game/chat/FriendInfo/FriendDuels.tsx
+++ src/UI/game/chat/FriendInfo/FriendDuels.tsx
@@ -7,7 +7,7 @@ import { ProfileContext } from "@/UI/ActiveUserProvider"
 type Props = { id: string | null; className: string }
 
 export default function FriendDuels({ id, className }: Props) {
-  const [duels, setDuels] = useState<MatchDisplayData[]>([])
+  const [duels, setDuels] = useState<MatchDisplayData[] | null>(null)
   const { profileData } = useContext(ProfileContext)
 
   useEffect(() => {
@@ -29,11 +29,15 @@ export default function FriendDuels({ id, className }: Props) {
 
   return (
     <div className={className}>
-      <p className="my-4 text-gray-300 text-base font-semibold ">Duels</p>
-      <div className="flex flex-col gap-2 overflow-y-scroll">
-        {duels.map((item) => {
-          return <DuelCard key={item.id} data={item} />
-        })}
+      <p className="my-4 text-gray-300 text-base font-semibold">Duels</p>
+      <div className="flex flex-col gap-2 overflow-y-scroll animate__animated animate__fadeIn">
+        {duels && duels.length > 0 ? (
+          duels.map((item) => <DuelCard key={item.id} data={item} />)
+        ) : duels && duels.length === 0 ? (
+          <p className="text-center font-light">no match found</p>
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   )

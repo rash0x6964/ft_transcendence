7fde1fa42d60a87f15d77a4cb5cc2335b3197326
rash0x6964 <rash0x6964@gmail.com>
2023-11-18 17:18:02 +0100

    fetch duels match at DM

--- src/UI/game/chat/FriendInfo/FriendDuels.tsx
+++ src/UI/game/chat/FriendInfo/FriendDuels.tsx
@@ -1,18 +1,37 @@
-import PropWithClass from "@/types/PropWithClass"
 import DuelCard from "./DuelCard"
+import matchService from "@/services/MatchService"
+import { useContext, useEffect, useState } from "react"
+import MatchDisplayData from "@/types/MatchDisplayData"
+import { ProfileContext } from "@/UI/ActiveUserProvider"
 
-export default function FriendDuels({ className }: PropWithClass) {
+type Props = { id: string; className: string }
 
-  
+export default function FriendDuels({ id, className }: Props) {
+  const [duels, setDuels] = useState<MatchDisplayData[]>([])
+  const { profileData } = useContext(ProfileContext)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const matchs = await matchService.getVsMatchesByIdByOffset(id, 0)
+        const matchDisplayData = await matchService.getMatchProps(
+          profileData,
+          matchs
+        )
+        setDuels(matchDisplayData)
+      } catch (error) {}
+    }
+
+    fetchData()
+  }, [id])
 
   return (
     <div className={className}>
       <p className="my-4 text-gray-300 text-base font-semibold ">Duels</p>
       <div className="flex flex-col gap-2 overflow-y-scroll">
-        {
-
-        }
-        <DuelCard scoreLeft={4} scoreRight={2} time={14} />
+        {duels.map((item) => {
+          return <DuelCard key={item.id} data={item} />
+        })}
       </div>
     </div>
   )

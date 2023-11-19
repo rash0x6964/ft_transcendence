5d94b7a6ac8e73d37de2d7aa958bc7c0deace4e7
Hatim Rachid <hat83203@gmail.com>
2023-11-19 21:05:14 +0100

    modifying sound button

--- src/UI/Layout.tsx
+++ src/UI/Layout.tsx
@@ -23,12 +23,12 @@ export default function Layout({ children }: PropsWithChildren) {
                 <MainNavBar className=" w-screen" coins={8000} />
                 <div className="flex-1 flex justify-between mb-4">
                   <LeftBar className=" h-full w-fit  z-0   bg-secondary mx-2 py-6 drop-shadow-lg  mb-4 rounded-lg flex flex-col  justify-between" />
-                  <div className=" flex-1 relative z-10">
+                  <div className=" flex-1 relative">
                     <div className="absolute w-full h-full max-h-full overflow-y-auto">
                       {children}
                     </div>
                   </div>
-                  <RightBar className="shadow z-0 h-full drop-shadow-lg   w-16 bg-secondary mx-2 mb-4 rounded-lg flex flex-col items-center gap-5 py-5" />
+                  <RightBar className="shadow  h-full drop-shadow-lg   w-16 bg-secondary mx-2 mb-4 rounded-lg flex flex-col items-center gap-5 py-5" />
                 </div>
               </div>
             </main>

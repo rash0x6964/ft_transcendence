ca73904aec969a38aacb4f6eb1e8b2fd03ad8493
rash0x6964 <rash0x6964@gmail.com>
2023-11-24 23:41:57 +0100

    finish game preferences

--- src/pages/settings/game.tsx
+++ src/pages/settings/game.tsx
@@ -1,24 +1,56 @@
 import SettingLayout from "@/UI/SettingLayout"
-import React, { ReactElement, useEffect, useState } from "react"
+import React, { ReactElement, useEffect, useMemo, useState } from "react"
 import { NextPageWithLayout } from "../_app"
 import ItemCard from "@/UI/game/store/ItemCard"
 import Product from "@/models/Product.model"
 import ProductService from "@/services/ProductService"
 import OwnedProduct from "@/components/svgs/OwnedProduct"
 import MainButton from "@/components/BaseComponents/MainButton"
+import RepoService from "@/services/RepoService"
 
 const Page: NextPageWithLayout = () => {
-  const [items, setItem] = useState<Product[]>([])
+  const mainPad: any = {
+    category: "PADDLE",
+    color: "#9BECE3",
+    id: "9f3eU400",
+    img: "",
+    name: "Aqua",
+    owned: true,
+    price: 0,
+  }
+
+  const mainMap: any = {
+    category: "MAPSKIN",
+    color: "",
+    id: "9f3eU4034",
+    img: "",
+    name: "blank",
+    owned: true,
+    price: 0,
+  }
+
+  const [items, setItem] = useState<Product[]>([mainPad, mainMap])
   const [selectedMap, setSelectedMap] = useState<Product | null>(null)
   const [selectedPad, setSelectedPad] = useState<Product | null>(null)
 
   useEffect(() => {
-    ProductService.getProductList()
-      .then((res) => {
-        setItem(res.data)
-        console.log(res.data)
-      })
-      .catch((err) => {})
+    const fetchData = async () => {
+      try {
+        const products: any = await ProductService.getProductList()
+        const _items = [mainPad, mainMap].concat(products.data)
+        const repo: any = await RepoService.getSelectedProduct()
+
+        setSelectedMap(
+          _items.find((x) => x.id === repo.data.selectedMapSkin) ?? mainMap
+        )
+        setSelectedPad(
+          _items.find((x) => x.id === repo.data.selectedPaddle) ?? mainPad
+        )
+
+        setItem(_items)
+      } catch (error) {}
+    }
+    fetchData()
   }, [])
 
   const itemClicked = (item: Product) => {
@@ -26,14 +58,28 @@ const Page: NextPageWithLayout = () => {
     else setSelectedPad(item)
   }
 
-  const saveMapSkin = () => {}
-  const savePadSkin = () => {}
+  const saveMapSkin = () => {
+    RepoService.updateRepo({
+      selectedMapSkin: selectedMap?.id,
+    })
+      .then((res) => {})
+      .catch((err) => {})
+  }
+
+  const savePadSkin = () => {
+    const body: any = {
+      selectedPaddle: selectedPad?.id,
+    }
+    RepoService.updateRepo(body)
+      .then((res) => {})
+      .catch((err) => {})
+  }
 
   return (
     <div className="flex flex-col gap-4">
       <div className="bg-secondary rounded-xl gradient-border-2 drop-shadow-md p-4">
         <p>Paddle skin</p>
-        <div className="flex p-4">
+        <div className="flex p-4 gap-5 flex-wrap">
           {items.map((item) => {
             if (item.owned && item.category === "PADDLE")
               return (
@@ -56,7 +102,7 @@ const Page: NextPageWithLayout = () => {
       </div>
       <div className="bg-secondary rounded-xl gradient-border-2 drop-shadow-md p-4">
         <p>Map skin</p>
-        <div className="flex p-4">
+        <div className="flex p-4 gap-5 flex-wrap">
           {items.map((item) => {
             if (item.owned && item.category === "MAPSKIN")
               return (

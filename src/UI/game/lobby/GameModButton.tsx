14d2839fd73945cb17012ad628ea5cfb6332a0b5
Hatim Rachid <hat83203@gmail.com>
2023-12-02 18:31:43 +0100

    adding info to gameModButton

--- src/UI/game/lobby/GameModButton.tsx
+++ src/UI/game/lobby/GameModButton.tsx
@@ -1,3 +1,4 @@
+import Info from "@/components/svgs/Info"
 import Image from "next/image"
 import React from "react"
 type Props = {
@@ -6,6 +7,7 @@ type Props = {
   gameMod: string
   img: string
   onClick: () => void
+  onInfoClick?: () => void
   disabled: boolean
 }
 export default function GameModButton({
@@ -14,6 +16,7 @@ export default function GameModButton({
   gameMod,
   img,
   onClick,
+  onInfoClick,
   disabled = false,
 }: Props) {
   return (
@@ -30,14 +33,22 @@ export default function GameModButton({
         className="top-0 left-0 absolute object-cover w-full h-full  rounded-xl"
         alt={gameMod}
       />
+
       <div
         className={`w-full h-full rounded-xl top-0 left-0 blur  absolute border-2 border-primary  ${
           selected ? "border-primary" : "border-transparent"
         } `}
       ></div>
+
       <div
         className={`w-full h-full rounded-xl flex flex-col justify-center top-0 left-0   absolute bg-gray-900/60 `}
       >
+        <button
+          onClick={onInfoClick}
+          className="absolute right-0 top-0 m-1  hover:text-white/50  text-white "
+        >
+          <Info width={20} height={20} className="opacity-100" />
+        </button>
         <h1 className=" w-full   text-xs ">{gameMod}</h1>
       </div>
     </button>

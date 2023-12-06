22f9ed2c529af7901ad33fdf840d373b0c2f6d0b
med-ghali <tagemouati.medghali@gmail.com>
2023-12-06 16:45:57 +0100

    revert lint

--- src/UI/game/lobby/GameModButton.tsx
+++ src/UI/game/lobby/GameModButton.tsx
@@ -1,14 +1,14 @@
-import Info from "@/components/svgs/Info";
-import React from "react";
+import Info from "@/components/svgs/Info"
+import React from "react"
 type Props = {
-  className?: string;
-  selected: Boolean;
-  gameMod: string;
-  img: string;
-  onClick: () => void;
-  onInfoClick?: () => void;
-  disabled: boolean;
-};
+  className?: string
+  selected: Boolean
+  gameMod: string
+  img: string
+  onClick: () => void
+  onInfoClick?: () => void
+  disabled: boolean
+}
 
 export default function GameModButton({
   className,
@@ -33,11 +33,13 @@ export default function GameModButton({
         className="top-0 left-0 absolute object-cover w-full h-full  rounded-xl"
         alt={gameMod}
       />
+
       <div
         className={`w-full h-full rounded-xl top-0 left-0 blur  absolute border-2 border-primary  ${
           selected ? "border-primary" : "border-transparent"
         } `}
       ></div>
+
       <div
         className={`w-full h-full rounded-xl flex flex-col justify-center top-0 left-0   absolute bg-gray-900/60 `}
       >
@@ -50,5 +52,5 @@ export default function GameModButton({
         <h1 className=" w-full   text-xs ">{gameMod}</h1>
       </div>
     </button>
-  );
+  )
 }

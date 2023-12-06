4fee6ca5388a59ec2a4c7f014e090979746bd55e
Hatim Rachid <hat83203@gmail.com>
2023-12-06 18:42:31 +0100

    addign docker ignore next js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
 	reactStrictMode:false,
 	output:"export",
+	eslint: {
+		// Warning: This allows production builds to successfully complete even if
+		// your project has ESLint errors.
+		ignoreDuringBuilds: true,
+	  },
 	images: {
 		unoptimized:true,
 	  remotePatterns: [

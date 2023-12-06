346a713c939bf8014820ef66b58086f0162ab755
Hatim Rachid <hat83203@gmail.com>
2023-12-06 21:38:57 +0100

    removing exports

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode:false,
-	output:"export",
 	eslint: {
 		// Warning: This allows production builds to successfully complete even if
 		// your project has ESLint errors.
